4640d1bcf6048a8c3ea40659ab3ef18bd8d725fe
Toshiba <Toshiba@TOSHIBAPC>
2017-01-12 16:12:19 +0100

    blog overview plus its controller etc added

---
+++ src/HoneymoonShop/Controllers/BlogsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HoneymoonShop.Data;
+using HoneymoonShop.Models;
+
+namespace HoneymoonShop.Controllers
+{
+    public class BlogsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BlogsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Blog
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
